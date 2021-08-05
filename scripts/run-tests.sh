257193bda43ec14f4e74cd75dfed75f11d042497
Andrew Robinson <andrewmcrobinson@gmail.com>
2021-08-05 17:35:56 +0200

    add cover

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,8 @@
 
  cd /go-with-compose
 # https://blog.jbowen.dev/2019/08/using-go-flags-in-tests/
-go test -v ./... -endpoint=http://accountapi:8080/v1/organisation/accounts
+# https://ieftimov.com/post/testing-in-go-go-test/
+go test ./... -cover -v -endpoint=http://accountapi:8080/v1/organisation/accounts
 
 #  go run cmd/main.go --endpoint=http://accountapi:8080/v1/organisation/accounts
 
